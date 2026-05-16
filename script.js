7077769e44f4383c38b47c01e9062c5a32abb987
Prometheuse1 <zakariaehasnicerdelec@gmail.com>
2026-05-16 13:45:42 +0100

    localhost

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ const password=document.getElementById('password');
 const button=document.getElementById('btn');
 
 button.addEventListener('click', function() {
+    const name=nom.value;
+    const mail=email.value;
+    const pass=password.value;
+
     if (nom.value==='' || email.value==='' || password.value==='') 
     {
         alert('Veuillez remplir tous les champs');
     } 
-    else{
+    else
+    {
+        const users=JSON.parse(localStorage.getItem('users') || '[]');
+        users.push({nom: name,email: mail,password: pass})
+        localStorage.setItem('users', JSON.stringify(users))
+
         alert('Inscription réussie !' + "nom : " + nom.value + " email : " + email.value + " password : " + password.value);
+        nom.value=email.value=password.value='';
     }
 });
